e8eb41a3d2cc56cfdf926c4c421c25d9618b4daf
Syrus Farris <syrious@Syruss-MacBook-Pro.local>
2022-09-23 14:08:47 -0500

    added questions to the question array that will help fill out the readme template

--- Develop/index.js
+++ Develop/index.js
@@ -17,6 +17,129 @@ const questions = [
                 return false;
             }
         }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub Username',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            }else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            }else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'why',
+        message: 'What is your project and what will you solve with it?',
+        validate: whyInput => {
+            if (whyInput) {
+                return true;
+            } else {
+                console.log('Please detail your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'use',
+        message: 'How will someone use this?',
+        validate: useInput => {
+            if (useInput) {
+                return true;
+            }else {
+                console.log('Please enter how this will be used!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide instructions on how to install your project.',
+        validate: installInput => {
+             if(installInput) {
+                 return true;
+            } else {
+                console.log('Please enter your installation instructions!');
+            return false;
+            }
+        }   
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions and examples for use. (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter your use instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your project?',
+        choices: ['agpl', 'apache', 'mit', 'no license']
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContributers',
+        message: 'Would you like to allow other developers to contribute?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please detail on how to contribute. (Required)',
+        when: ({ confirmContributers }) => {
+            if (confirmContributers) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributerInput => {
+            if (contributerInput) {
+                return true;
+            } else {
+                console.log('Please enter contributer guidelines!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please detail on how to test the app. (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter your use test instructions!');
+                return false;
+            }
+        }
     }
 ];
 
